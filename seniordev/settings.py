7315589b4beb06b009eccb0b32d5f9e5209c49fb
Awmusic12635 <alex@alexwacker.com>
2017-02-04 11:38:32 -0500

    test

--- seniordev/settings.py
+++ seniordev/settings.py
@@ -78,13 +78,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': os.getenv('DB_NAME','circle_test'),
-        'USER': os.getenv('DB_USER','ubuntu'),
+        'USER': os.getenv('DB_USER','root'),
         'PASSWORD': os.getenv('DB_PASS',''),
         'HOST': os.getenv('DB_HOST','127.0.0.1'),
         'PORT': os.getenv('DB_PORT','3306'),
-        'TEST': {
-            'NAME': 'circle_test',
-        },
     }
 }
 
