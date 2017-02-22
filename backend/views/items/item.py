d8ee4e5e3cc0b40be2fd6707add3be26609a5fd1
Alex Wacker <alex@alexwacker.com>
2017-02-21 19:43:04 -0500

    Yay now editing works too

--- backend/views/items/item.py
+++ backend/views/items/item.py
@@ -25,13 +25,21 @@ def add_item(request):
 
 
 @login_required
-def show_item(request, id):
-    item = get_object_or_404(Item, pk=id)
+def show_item(request, item_id):
+    item = get_object_or_404(Item, pk=item_id)
 
     return render(request, 'itemDetailed.html', {'title': item.name, 'item': item})
 
-@login_required
-def edit_item(request,id):
-    item = get_object_or_404(Item, pk=id)
 
-    return render(request, 'editItem.html', {'title': "Edit: " + item.name, 'item': item})
+@login_required
+def edit_item(request, item_id):
+    item = get_object_or_404(Item, pk=item_id)
+    if request.method == "POST":
+        form = ItemForm(request.POST, instance=item)
+        if form.is_valid():
+            form.save()
+            # for now redirect back to item listings. Until detailed page is done
+            return redirect('itemList')
+    else:
+        form = ItemForm(instance=item)
+        return render(request, 'editItem.html', {'title': "Edit: " + item.name, 'form': form, 'item': item})
