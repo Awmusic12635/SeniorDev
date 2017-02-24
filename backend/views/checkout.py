680691eb9a7d438c26a6d8906ab6132e62ec4642
Ted Fitzgerald <ehcfitzgerald@gmail.com>
2017-02-23 19:38:21 -0500

    debug logging

--- backend/views/checkout.py
+++ backend/views/checkout.py
@@ -2,19 +2,25 @@ from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponseRedirect, HttpResponse
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
+import logging
 
 from backend.models import Checkout
 from backend.models import CheckoutItem
 from django.core.exceptions import ObjectDoesNotExist
 
 CONST_STATUS_PENDING = "Pending"
+logger = logging.getLogger('ted')
 
 @login_required
 def get_pending_checkout(request):
     #get pending checkout if there is one
-    checkout = Checkout.objects.filter(status=CONST_STATUS_PENDING)[:1]
+    checkout = Checkout.objects.filter(status=CONST_STATUS_PENDING)
+    logger.debug('old'+ checkout)
+    logger.debug('old'+ checkout.id)
     if checkout is None:
         checkout = Checkout(status = CONST_STATUS_PENDING)
         checkout.save()
+        logger.debug('new' +checkout)
+        logger.debug('new' +checkout.id)
     return render(request, 'checkout.html', {'title': 'Checkout', 'checkout': checkout})
 
