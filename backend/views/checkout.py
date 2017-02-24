a243f75bce5bce23158a69f74832cf0cc67d5cfd
Ted Fitzgerald <ehcfitzgerald@gmail.com>
2017-02-23 21:02:26 -0500

    remove item from checkout

--- backend/views/checkout.py
+++ backend/views/checkout.py
@@ -11,11 +11,11 @@ CONST_STATUS_CHECKEDIN = "Checked in"
 
 @login_required
 def get_pending_checkout(request):
-    return render(request, 'checkout.html', {'title': 'Checkout', 'checkout': get_pending_checkout})
+    return render(request, 'checkout.html', {'title': 'Checkout', 'checkout': get_pending_checkout()})
 
 
 def add_item(request, item_id):
-    checkout = get_pending_checkout
+    checkout = get_pending_checkout()
     item = get_object_or_404(Item, pk=int(item_id))
     ci = CheckoutItem(checkout = checkout, item = item)
     ci.save()
@@ -28,7 +28,7 @@ def add_item(request, item_id):
 
 def remove_item(request, item_id):
     item = get_object_or_404(Item, pk=int(item_id))
-    checkout = get_pending_checkout
+    checkout = get_pending_checkout()
     ci = CheckoutItem.objects.filter(item=item, checkout=checkout)
     ci.delete()
 
