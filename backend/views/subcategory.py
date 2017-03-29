7a3773a16f4e525541c7d5f3e055ba181e94ae25
ehf5307 <ehcfitzgerald@gmail.com>
2017-03-28 22:59:19 -0400

    renaming this file got all sorts of fucked up

--- backend/views/subcategory.py
+++ backend/views/subcategory.py
@@ -3,8 +3,8 @@ from django.http import HttpResponseRedirect, HttpResponse
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
 from backend.forms import ItemSubCategoryForm
-from backend.models import ItemSubCategory
-
+from backend.models import ItemSubCategory, ItemCategory
+from backend.views import category
 
 @login_required
 def list_subcategories(request):
@@ -19,27 +19,42 @@ def view_subcategory(request, subcategory_id):
 
 
 @login_required
-def add_subcategory(request):
+def add_subcategory(request, category_id=None):
     if request.method == "POST":
         form = ItemSubCategoryForm(request.POST)
         if form.is_valid():
             form.save()
             # for now redirect back to the same page
-            return redirect('subcategoryList')
+            if category_id is None:
+                return redirect('subcategoryList')
+            else:
+                return redirect('categoryView')
+                #return category.view_category(request, category_id)
     else:
-        form = ItemSubCategoryForm()
-        return render(request, 'addSubCategory.html', {'title': 'Add Sub Category', 'form': form})
+        sub = ItemSubCategory
+        if category_id is not None:
+            sub.itemCategoryID = get_object_or_404(ItemCategory, pk= category_id)
+        form = ItemSubCategoryForm(instance=sub)
+        return render(request, 'addSubCategory.html', {'title': 'Add Sub Category', 'form': form, 'categoryID':category_id})
 
 
 @login_required
-def edit_subcategory(request,subcategory_id):
+def edit_subcategory(request,subcategory_id, category_id=None):
     subcat = get_object_or_404(ItemSubCategory, pk=subcategory_id)
     if request.method == "POST":
         form = ItemSubCategoryForm(request.POST, instance=subcat)
         if form.is_valid():
             form.save()
             # for now redirect back to item listings. Until detailed page is done
-            return redirect('subcategoryList')
+            if category_id is None:
+                return redirect('subcategoryList')
+            else:
+                return redirect('categoryView')
     else:
         form = ItemSubCategoryForm(instance=subcat)
-        return render(request, 'editSubCategory.html', {'title': "Edit: " + subcat.subCategoryName, 'form': form, 'subcategory': subcat, })
+        action = ''
+        if category_id is None:
+            action = '/subcategory/' + subcat.id + '/edit'
+        else:
+            action = '/subcategory/' +category_id+'/'+ subcat.id + '/edit'
+        return render(request, 'editSubCategory.html', {'title': "Edit: " + subcat.subCategoryName, 'form': form, 'subcategory': subcat, 'action':action })
