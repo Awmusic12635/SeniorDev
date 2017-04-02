bf17e7a234b60c5e657d6c86dcb751def67133b2
Edward Fitzgerald <ehcfitzgerald@gmail.com>
2017-04-02 16:01:19 -0400

    starting email messages for reservation requests

--- seniordev/settings.py
+++ seniordev/settings.py
@@ -130,4 +130,8 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(REPOSITORY_ROOT, 'media/')
 
+EMAIL_HOST=os.getenv('EMAIL_HOST')
+EMAIL_PORT=os.getenv('EMAIL_PORT')
+EMAIL_HOST_USER=os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD=os.getenv('EMAIL_HOST_PASSWORD')
 
