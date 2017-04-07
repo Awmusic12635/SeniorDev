2db2c205f7f0547b4a7906202efdd0af097abfd8
Ted Fitzgerald <ehcfitzgerald@gmail.com>
2017-04-07 12:14:18 -0400

    wrong import statement

--- backend/models.py
+++ backend/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from model_utils.models import TimeStampedModel
-from model_utils.models import FieldTracker
+from model_utils import FieldTracker
 
 #classes with no FKs
 
