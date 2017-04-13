91497fd165e99dd526ee46fef4e71d79963697ab
Ted Fitzgerald <ehcfitzgerald@gmail.com>
2017-04-13 18:10:33 -0400

    starting to try user lookup for chekout - i have no idea what im doing

--- backend/urls.py
+++ backend/urls.py
@@ -41,7 +41,7 @@ urlpatterns = [
     url(r'^search$', user.index, name='items'),
     #checkout
     url(r'^checkout/$', checkout.get_pending_checkout, name='items'),
-    url(r'^checkout/findUser/(?P<username>\d*)$', checkout.find_user, name='items'),
+    url(r'^checkout/findUser/(?P<username>\S*)$', checkout.find_user, name='items'),
     url(r'^checkout/addItem/(?P<item_id>\d*)$', checkout.add_item, name='items'),
     url(r'^checkout/removeItem/(?P<item_id>\d*)$', checkout.remove_item, name='items'),
     url(r'^checkout/resetDueDate/(?P<checkoutitem_id>\d*)$', checkout.reset_duedate, name='items'),
