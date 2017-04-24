0904a87d1b233b88e87be64c64cde9c7f929e1a6
Ted Fitzgerald <ehcfitzgerald@gmail.com>
2017-04-24 19:39:25 -0400

    user dropdown for login - not our view so using ajax

--- backend/views/user.py
+++ backend/views/user.py
@@ -3,17 +3,22 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from backend.models import User
+import json
 
 def index(request):
     return HttpResponse("Hello, world. You're at the user index.")
 
 
 def login(request):
-    staff = User.objects.all().filter(is_staff=1)
-    print(staff)
-    return render(request, 'login.html', {'staff':staff})
+    return render(request, 'login.html', {})
 
 
 @login_required
 def dashboard(request):
     return render(request, 'dashboard.html', {'title': 'Dashboard'})
+
+
+def get_staff(request):
+    staff = User.objects.all().filter(is_staff=1)
+    print(staff)
+    return HttpResponse(json.dumps({'users': staff}), content_type="application/json")
