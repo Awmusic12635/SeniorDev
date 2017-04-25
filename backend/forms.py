d3fbae6dd5eed99af15b975d5ca48452010ae0b8
Ted Fitzgerald <ehcfitzgerald@gmail.com>
2017-04-24 20:21:09 -0400

    remove itemtype field from item form because it is being overidded by request

--- backend/forms.py
+++ backend/forms.py
@@ -5,7 +5,7 @@ from .models import Item, ItemCategory, ItemSubCategory, Checkout, CheckoutItem,
 class ItemForm(forms.ModelForm):
     class Meta:
         model = Item
-        fields = ('serial', 'tag', 'location', 'ItemTypeID')
+        fields = ('serial', 'tag', 'location')
 
 
 class ItemTypeForm(forms.ModelForm):
